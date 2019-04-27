629a9477718a5a7f3ff24435e10ab8abb5c2abda
SohamGlobal <sohamglobal@users.noreply.github.com>
2019-04-27 18:52:17 +0530

    Add files via upload

---
+++ Users.java
@@ -0,0 +1,33 @@
+package global.services;
+
+public class Users 
+{
+	private String userid;
+	private String pass;
+	
+	public Users()
+	{
+		userid="";
+		pass="";
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+	
+
+}
